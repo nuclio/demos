3f20881c91b95adbba03c795579abe253e043f8b
Eran Duchan <pavius@gmail.com>
2018-01-17 17:51:52 +0200

    add copyright

--- fleet-alarm-detection-azure/sensor-handler.go
+++ fleet-alarm-detection-azure/sensor-handler.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2017 The Nuclio Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 // @nuclio.configure
 //
 // function.yaml:
@@ -168,7 +184,7 @@ func callFunction(context *nuclio.Context, name string, body interface{}) ([]byt
     
     // convert function name to URL
     // url := fmt.Sprintf("http://%s.default.svc.cluster.local", name)
-    url := fmt.Sprintf("http://172.17.0.1:37357")
+    // url := fmt.Sprintf("http://172.17.0.1:<local port>")
 
     response, err := http.Post(url, "content-type/json", bytes.NewBuffer(marshalledBody))
     if err != nil {
