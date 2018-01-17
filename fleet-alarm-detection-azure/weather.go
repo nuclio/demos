02d18130b281949c60195825101efdbf6d0ddb77
Eran Duchan <pavius@gmail.com>
2018-01-17 17:54:31 +0200

    Initial commit (#1)

---
+++ fleet-alarm-detection-azure/weather.go
@@ -0,0 +1,68 @@
+/*
+Copyright 2017 The Nuclio Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+    "encoding/json"
+    "net/http"
+    "math/rand"
+
+    "github.com/nuclio/nuclio-sdk"
+)
+
+type weather struct {
+    Temperature int `json:"temperature"`
+    WeatherCondition string `json:"weatherCondition"`
+}
+
+func Handler(context *nuclio.Context, event nuclio.Event) (interface{}, error) {
+    context.Logger.InfoWith("Received event", "body", string(event.GetBody()))
+
+    weatheritem := &weather{
+        Temperature: randInt(-10,50),
+        WeatherCondition: randWeatherCondition(),
+    }
+    
+    json, err := json.Marshal(weatheritem)
+    if err != nil {
+        return nil, err
+    }
+
+    // return the contents as JSON
+    return nuclio.Response{
+        StatusCode:  http.StatusOK,
+        ContentType: "application/json",
+        Body:        []byte(json),
+    }, nil
+}
+
+func randInt(min int, max int) int {
+    return min + rand.Intn(max-min)
+}
+
+func randWeatherCondition() string {
+    weatherConditions := []string{
+        "clear",
+        "cloudy",
+        "rain",
+        "snow",
+    }
+    
+    message := weatherConditions[rand.Intn(len(weatherConditions))]
+    
+    return message
+}
